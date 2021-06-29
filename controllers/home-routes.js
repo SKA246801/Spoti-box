30682c51da1858a6ca39c77213aead9d8132d819
JWD <johnwdevine85@gmail.com>
2021-06-29 18:24:01 -0500

    css

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.render('homepage')
+  res.render('homepage')
 })
 
 router.get('/login', (req, res) => {
-    var scopes = 'user-read-private user-read-email';
-res.redirect('https://accounts.spotify.com/authorize' +
+  // res.render('login')
+  var scopes = 'user-read-private user-read-email'
+  res.redirect('https://accounts.spotify.com/authorize' +
   '?response_type=code' +
   '&client_id=' + process.env.sometext4 +
   (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
